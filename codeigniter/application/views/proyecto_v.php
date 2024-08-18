71c36eae1366bed03824355370fc6fbd9c9f7cf4
ever calle <calle.ever.878@gmail.com>
2024-08-18 00:53:45 -0400

    modificacion de la tabla proyectos y ajustar diseño

--- codeigniter/application/views/proyecto_v.php
+++ codeigniter/application/views/proyecto_v.php
@@ -1,61 +1,95 @@
+<body>
+<?php $this->load->view('lateral_v'); ?> <!-- Incluye el menú lateral -->
 
+    <!-- Content Start -->
+    <div class="content container-fluid pt-4 px-4">
+        <?php $this->load->view('encabezado_v'); ?> <!-- Incluye el encabezado -->
+        <div class="row g-4">
+            <div class="col-12">
+                <div class="bg-secondary rounded h-100 p-4">
+                
+                
+                    <!-- Botón para agregar un nuevo proyecto -->
+                    <div class="col-12 mb-4">
+                        <a href="<?php echo site_url('Proyecto_c/agregar'); ?>" class="btn btn-primary">
+                            <i class="bi bi-plus"></i> Agregar Proyecto
+                        </a>
+                    </div>
+                    <!-- Tabla para mostrar los proyectos -->
+                    <div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <h6 class="mb-4">Listado de Proyectos</h6>
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Título</th>
+                                        <th>Egresado</th>
+                                        <th>Gestión</th>
+                                        <th>Resumen</th>
+                                        <th>Estado</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php if (!empty($proyectos)) { ?>
+                                        <?php foreach ($proyectos->result() as $proyecto) { ?>
+                                            <tr>
+                                                <td><?php echo $proyecto->idproyecto; ?></td>
+                                                <td><?php echo $proyecto->titulo; ?></td>
+                                                <td><?php echo $proyecto->egresado; ?></td>
+                                                <td><?php echo $proyecto->gestion; ?></td>
+                                                <td><?php echo $proyecto->resumen; ?></td>
+                                                <td><?php echo $proyecto->estado ? 'Activo' : 'Inactivo'; ?></td>
+                                                <td>
+                                                    <!-- Botones CRUD -->
+                                                    <a href="<?php echo site_url('Proyecto_c/modificar/'.$proyecto->idproyecto); ?>" class="btn btn-warning">
+                                                        <i class="bi bi-pencil-square"></i> Modificar
+                                                    </a>
+                                                    <a href="<?php echo site_url('Proyecto_c/ver/'.$proyecto->idproyecto); ?>" class="btn btn-info">
+                                                        <i class="bi bi-eye"></i> Ver
+                                                    </a>
+                                                    <form action="<?php echo site_url('Proyecto_c/eliminarbd'); ?>" method="post" style="display:inline;">
+                                                        <input type="hidden" name="idProyecto" value="<?php echo $proyecto->idproyecto; ?>">
+                                                        <button type="submit" class="btn btn-danger">
+                                                            <i class="bi bi-trash"></i> Eliminar
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    <?php } else { ?>
+                                        <tr>
+                                            <td colspan="7">No hay proyectos registrados.</td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div> <!-- Fin de .col-12 -->
+                </div> <!-- Fin de .bg-secondary -->
+            </div> <!-- Fin de .col-12 -->
+            <?php $this->load->view('pie_v'); ?> <!-- Incluye pie de página -->
+        </div> <!-- Fin de .row -->
+    </div> <!-- Fin de .content -->
 
-<!-- Content Start -->
-<div>
-    <div class="row g-4">
-        <!-- Botón para agregar un nuevo proyecto -->
-        <div class="col-12 mb-4">
-            <a href="<?php echo site_url('Proyecto_c/agregar'); ?>" class="btn btn-primary"><i class="bi bi-plus"></i> Agregar Proyecto</a>
-        </div>
-        <!-- Tabla para mostrar los proyectos -->
-        <div class="col-12">
-            <div class="bg-secondary rounded h-100 p-4">
-                <h6 class="mb-4">Listado de Proyectos</h6>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Título</th>
-                            <th>Egresado</th>
-                            <th>Gestión</th>
-                            <th>Resumen</th>
-                            <th>Estado</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                        <?php if (!empty($proyectos)) { ?>
-                            <?php foreach ($proyectos->result() as $proyecto) { ?>
-                                <tr>
-                                    <td><?php echo $proyecto->idproyecto; ?></td>
-                                    <td><?php echo $proyecto->titulo; ?></td>
-                                    <td><?php echo $proyecto->egresado; ?></td>
-                                    <td><?php echo $proyecto->gestion; ?></td>
-                                    <td><?php echo $proyecto->resumen; ?></td>
-                                    <td>
-                                        <?php echo $proyecto->estado ? 'Activo' : 'Inactivo'; ?>
-                                    </td>
-                                    <td>
-                                        <!-- Botones CRUD -->
-                                        <a href="<?php echo site_url('Proyecto_c/modificar/'.$proyecto->idproyecto); ?>" class="btn btn-warning"><i class="bi bi-pencil-square"></i> Modificar</a>
-                                        <a href="<?php echo site_url('Proyecto_c/ver/'.$proyecto->idproyecto); ?>" class="btn btn-info"><i class="bi bi-eye"></i> Ver</a>
-                                        <form action="<?php echo site_url('Proyecto_c/eliminarbd'); ?>" method="post" style="display:inline;">
-                                            <input type="hidden" name="idProyecto" value="<?php echo $proyecto->idproyecto; ?>">
-                                            <button type="submit" class="btn btn-danger"><i class="bi bi-trash"></i> Eliminar</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            <?php } ?>
-                        <?php } else { ?>
-                            <tr>
-                                <td colspan="7">No hay proyectos registrados.</td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Content End -->
+    
+
+    <!-- Volver al Inicio -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+    <!-- Bibliotecas de JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url('../template/lib/chart/chart.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/easing/easing.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/waypoints/waypoints.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/owlcarousel/owl.carousel.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/tempusdominus/js/moment.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/tempusdominus/js/moment-timezone.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/js/main.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/dist/js/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/dist/js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?php echo base_url('../template/dist/js/scripts.js'); ?>"></script>
+</body>
--- codeigniter/application/views/usuario_v.php
+++ codeigniter/application/views/usuario_v.php
@@ -1,103 +1,102 @@
-
-
-
 <body>
-    <?php $this->load->view('encabezado_v'); ?> <!-- Incluye el encabezado -->
-    <?php $this->load->view('lateral_v'); ?> <!-- Incluye el menú lateral -->
+    <!-- Incluye el menú lateral -->
+    <?php $this->load->view('lateral_v'); ?>
 
-        <!-- Barra de navegación -->
-        <div class="content container-fluid pt-4 px-4">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Gestión de Usuarios</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/login_c/logout">
-                        <button type="button" class="btn btn-warning">Cerrar Sesión</button>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/login_c/panel">
-                        <button type="button" class="btn btn-warning">Volver</button>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url();?>index.php/usuario_c/agregar">
-                        <button type="button" class="btn btn-primary">Agregar Usuario</button>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    
-    <!-- Contenido principal -->
- 
-    <div class="container-fluid pt-4 px-4">
-                <div class="row g-4">
-                    <div class="col-12">
-                        <div class="bg-secondary rounded h-100 p-4">
-                            <h6 class="mb-4">Responsive Table</h6>
-                            <div class="table-responsive">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                        <th>#</th>
-                                            <th>Nombres</th>
-                                            <th>Primer Apellido</th>
-                                            <th>Segundo Apellido</th>
-                                            <th>Correo</th>
-                                            <th>Teléfono</th>
-                                            <th>Nombre Usuario</th>
-                                            <th>Contraseña</th>
-                                            <th>Estado</th>
-                                            <th>Acciones</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    <?php
-                                        $contador = 1;
-                                        foreach ($usuario->result() as $row) {
-                                        ?>
-                                        <tr>
-                                            <td><?php echo $contador; ?></td>
-                                            <td><?php echo $row->nombres; ?></td>
-                                            <td><?php echo $row->primerApellido; ?></td>
-                                            <td><?php echo $row->segundoApellido; ?></td>
-                                            <td><?php echo $row->correo; ?></td>
-                                            <td><?php echo $row->telefono; ?></td>
-                                            <td><?php echo $row->nombreUsuario; ?></td>
-                                            <td><?php echo $row->contrasena; ?></td>
-                                            <td><?php echo $row->estado; ?></td>
-                                            <td>
-                                                <?php echo form_open("usuario_c/modificar"); ?>
-                                                <input type="hidden" name="idusuario" value="<?php echo $row->idusuario; ?>">
-                                                <button type="submit" class="btn btn-warning btn-sm">Editar</button>
-                                                <?php echo form_close(); ?>
-                                            </td>
-                                            <td>
-                                                <?php echo form_open("usuario_c/eliminarbd"); ?>
-                                                <input type="hidden" name="idusuario" value="<?php echo $row->idusuario; ?>">
-                                                <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
-                                                <?php echo form_close(); ?>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                        $contador++;
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
+    <div class="content container-fluid px-4">
+        <!-- Incluye el encabezado -->
+        <?php $this->load->view('encabezado_v'); ?>
+
+        <!-- Contenido principal -->
+        <div class="row g-0">
+            <div class="col-12">
+                <div class="bg-secondary rounded h-100 p-4">
+                    <!-- Barra de navegación -->
+                    <nav class="navbar navbar-expand-lg navbar-light mb-4">
+                        <a class="navbar-brand" href="#">Gestión de Usuarios</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/login_c/logout">
+                                        <button type="button" class="btn btn-warning">Cerrar Sesión</button>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/login_c/panel">
+                                        <button type="button" class="btn btn-warning">Volver</button>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url(); ?>index.php/usuario_c/agregar">
+                                        <button type="button" class="btn btn-primary">Agregar Usuario</button>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
+                    </nav>
+
+                    <!-- Tabla de usuarios -->
+                    <h6 class="mb-4">Responsive Table</h6>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nombres</th>
+                                    <th>Primer Apellido</th>
+                                    <th>Segundo Apellido</th>
+                                    <th>Correo</th>
+                                    <th>Teléfono</th>
+                                    <th>Nombre Usuario</th>
+                                    <th>Contraseña</th>
+                                    <th>Estado</th>
+                                    <th colspan="2">Acciones</th> <!-- Combine columns for actions -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $contador = 1;
+                                foreach ($usuario->result() as $row) {
+                                ?>
+                                    <tr>
+                                        <td><?php echo $contador; ?></td>
+                                        <td><?php echo $row->nombres; ?></td>
+                                        <td><?php echo $row->primerApellido; ?></td>
+                                        <td><?php echo $row->segundoApellido; ?></td>
+                                        <td><?php echo $row->correo; ?></td>
+                                        <td><?php echo $row->telefono; ?></td>
+                                        <td><?php echo $row->nombreUsuario; ?></td>
+                                        <td><?php echo $row->contrasena; ?></td>
+                                        <td><?php echo $row->estado; ?></td>
+                                        <td>
+                                            <?php echo form_open("usuario_c/modificar"); ?>
+                                            <input type="hidden" name="idusuario" value="<?php echo $row->idusuario; ?>">
+                                            <button type="submit" class="btn btn-warning btn-sm">Editar</button>
+                                            <?php echo form_close(); ?>
+                                        </td>
+                                        <td>
+                                            <?php echo form_open("usuario_c/eliminarbd"); ?>
+                                            <input type="hidden" name="idusuario" value="<?php echo $row->idusuario; ?>">
+                                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                            <?php echo form_close(); ?>
+                                        </td>
+                                    </tr>
+                                <?php
+                                $contador++;
+                                }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-            <!-- Fin de Carreras -->
-            <?php $this->load->view('pie_v'); ?> <!-- Incluye pie de página -->
         </div>
+
+        <!-- Incluye pie de página -->
+        <?php $this->load->view('pie_v'); ?>
+
     </div>
 
     <!-- Volver al Inicio -->
@@ -118,5 +117,3 @@
     <script src="<?php echo base_url('../template/dist/js/bootstrap.bundle.min.js'); ?>"></script>
     <script src="<?php echo base_url('../template/dist/js/scripts.js'); ?>"></script>
 </body>
-
-        
